3bb3bc586111ed2c90c9c5cfd03e5b734046696f
Lewis-Hellewell <imlewishellewell@gmail.com>
2022-09-10 13:58:38 +0800

    color mapping and options

--- src/constants.py
+++ src/constants.py
@@ -7,3 +7,8 @@
 # The types of algorithms that can be used to generate the initial graph topology
 ERDOS_RENYI = 1
 BARABASI_ALBERT = 2
+
+# Color mapping types
+MAP_TEAMS = 1
+MAP_WILLVOTE = 2
+MAP_UNCERTAINTY = 3
